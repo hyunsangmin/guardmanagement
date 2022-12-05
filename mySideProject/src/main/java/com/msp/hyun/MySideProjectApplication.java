b1a22c9ef7e38da6861854e61d16906ce327e95f
hyunsangmin <hyunsm1698@naver.com>
2022-12-05 17:35:41 +0900

    클릭하면 정보랑 디테일까지 가능 geoserver연동해야함

--- mySideProject/src/main/java/com/msp/hyun/MySideProjectApplication.java
+++ mySideProject/src/main/java/com/msp/hyun/MySideProjectApplication.java
@@ -2,10 +2,16 @@ package com.msp.hyun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MySideProjectApplication {
-
+public class MySideProjectApplication  extends SpringBootServletInitializer{
+    // 이 부분 추가
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MySideProjectApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MySideProjectApplication.class, args);
 	}
